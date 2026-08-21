0dc21937f70a578584cc6005524bc519b10dc2be
xenoriqlabs <xenoriqlabs@gmail.com>
2026-08-21 16:16:01 +0500

    Initial Commit

---
+++ Phase 1 — Python Fundamentals/01-python-introduction.py
@@ -0,0 +1,88 @@
+# 1. What is Python?
+'''
+Python is a high-level, interpreted, general-purpose programming language created by Guido van Rossum and first released in 1991.
+Python is known for its simple, readable syntax, which makes it beginner-friendly while also being powerful enough for professional software development.
+'''
+
+# 2. Python Features
+
+'''
+Important features of Python include:
+
+--> Easy to learn — Simple and readable syntax.
+--> High-level language — You don't need to manage low-level computer operations.
+--> Interpreted — Python code is executed by the Python interpreter.
+--> Dynamically typed — You don't need to explicitly declare variable types.
+--> Object-oriented — Supports classes and objects.
+--> Cross-platform — Works on Windows, macOS, Linux, etc.
+--> Open-source — Python is freely available.
+--> Large standard library — Provides many built-in modules and tools.
+--> Large community — Has a huge ecosystem of developers and libraries.
+--> Extensible — Can work with languages such as C and C++.
+'''
+
+# 3. Python Use Cases
+
+'''
+Python is used in many areas of software development:
+
+--> Web Development — Django, Flask, FastAPI
+--> Artificial Intelligence — PyTorch, TensorFlow
+--> Machine Learning — Scikit-learn, XGBoost
+--> Data Science — Pandas, NumPy
+--> Data Visualization — Matplotlib, Seaborn
+--> Automation & Scripting — Automating repetitive tasks
+--> Web Scraping — BeautifulSoup, Scrapy
+--> Cybersecurity — Security tools and automation
+--> Desktop Applications — Tkinter, PyQt
+--> Game Development — Pygame
+--> Scientific Computing — NumPy, SciPy
+--> DevOps — Automation and infrastructure scripting
+--> Backend Development — APIs and server-side applications
+'''
+
+# 4. Python Versions
+
+'''
+Python has gone through several major versions.
+
+--> The two most important historical versions were:
+
+        Python 2.x
+        Python 3.x
+
+Python 2 reached its official end of life on January 1, 2020.
+Today, you should learn and use Python 3.x.
+'''
+
+# 5. Python 3.x
+
+'''
+Python 3 is the modern version of Python.
+
+Examples of Python 3 releases include:
+    Python 3.8
+    Python 3.9
+    Python 3.10
+    Python 3.11
+    Python 3.12
+    Python 3.13
+    Python 3.14
+
+The exact version you use may change over time, but the core Python concepts remain largely consistent.
+--> You can check your installed Python version using:
+    python --version
+'''
+
+# 6. Interpreter
+
+'''
+A Python interpreter is the software that executes Python code.
+
+For example, when you write:
+'''
+print("Hello, Word!")
+'''
+the Python interpreter reads and executes this code.
+The interpreter allows you to run Python programs without manually converting your Python source code into machine code yourself.
+'''
