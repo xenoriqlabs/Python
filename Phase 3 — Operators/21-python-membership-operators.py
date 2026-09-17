148b7510451258b5c224e89a3658d4e9e86ce49d
xenoriqlabs <xenoriqlabs@gmail.com>
2026-09-17 23:55:39 +0500

    Initial Commit

--- Phase 3 — Operators/21-python-membership-operators.py
+++ Phase 3 — Operators/21-python-membership-operators.py
@@ -54,3 +54,72 @@ print("S" in name)
 print("a" in name)
 print("z" in name)
 
+# ==========================================
+# 6. Tuple Membership
+# ==========================================
+
+languages = (
+    "python",
+    "C++",
+    "JavaScript",
+    "Java"
+)
+
+print("\nTuple Membership:")
+
+print("Python" in languages)
+print("C++" in languages)
+print("PHP" in languages)
+
+print("PHP" not in languages)
+print("Python" not in languages)
+
+# ==========================================
+# 7. Set Membership
+# ==========================================
+
+skills = {
+    "Python",
+    "C++",
+    "JavaScript",
+    "React",
+    "Node.js"
+}
+
+print("\nSet Membership:")
+
+print("Python" in skills)
+print("React" in skills)
+print("Flutter" in skills)
+
+print("Flutter" not in skills)
+
+# ==========================================
+# 8. Dictionary Membership
+# ==========================================
+
+student = {
+    "name": "Saif",
+    "age": 20,
+    "city": "Sahiwal"
+}
+
+print("\nDictionary Membership:")
+
+# 'in' checks dictionary keys.
+
+print("name" in student)
+print("age" in student)
+print("email" in student)
+
+
+# ==========================================
+# 9. Checking Dictionary Values
+# ==========================================
+
+print("\nDictionary Values:")
+
+print("Saif" in student.values())
+print("Lahore" in student.values())
+
+print("Lahore" not in student.values())
